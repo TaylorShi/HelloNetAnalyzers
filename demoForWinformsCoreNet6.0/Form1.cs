691a4c8f1994914d2662f49100014ea64a63d652
shiya <shiya@300264.cn>
2021-11-18 20:49:28 +0800

    1

---
+++ demoForWinformsCoreNet6.0/Form1.cs
@@ -0,0 +1,9 @@
+namespace demoForWinformsCoreNet6._0;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+}
---
+++ demoForWinformsCoreNet6.0/Program.cs
@@ -0,0 +1,14 @@
+namespace demoForWinformsCoreNet6._0;
+
+static class Program
+{
+    /// <summary>
+    ///  The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+        Application.Run(new Form1());
+    }    
+}
